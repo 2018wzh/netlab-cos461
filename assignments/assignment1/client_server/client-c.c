8b7c9ae9ffd27a82e07603a6fe37798c7edcbbf5
2018wzh <wzh20070720@outlook.com>
2025-10-09 19:08:41 +0800

    [lab-1] init task1

--- assignments/assignment1/client_server/client-c.c
+++ assignments/assignment1/client_server/client-c.c
@@ -1,41 +1,89 @@
 /*****************************************************************************
- * client-c.c                                                                 
+ * client-c.c
  * Name:
  * NetId:
  *****************************************************************************/
 
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <errno.h>
+#include <unistd.h>
 
 #define SEND_BUFFER_SIZE 2048
-
 
 /* TODO: client()
  * Open socket and send message from stdin.
  * Return 0 on success, non-zero on failure
-*/
+ */
 int client(char *server_ip, char *server_port) {
+  // Create socket
+  int port = atoi(server_port);
+  // Check port number
+  if (port <= 0 || port > 65535) {
+    fprintf(stderr, "Invalid port number: %s\n", server_port);
+    return -1;
+  }
+  int sockfd;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    perror("Error opening socket");
+    return -1;
+  }
+  // Get server address
+  struct hostent *server;
+  server = gethostbyname(server_ip);
+  if (server == NULL) {
+    fprintf(stderr, "Error, no such host: %s\n", server_ip);
+    return -1;
+  }
+  // Connect to server
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  memcpy(&serv_addr.sin_addr.s_addr, server->h_addr_list[0],
+         server->h_length); // Use first address
+  serv_addr.sin_port = htons(port);
+  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("Error connecting to server");
+    return -1;
+  }
+  // Read message from stdin and send to server
+  char buffer[SEND_BUFFER_SIZE];
+  memset(buffer, 0, SEND_BUFFER_SIZE);
+  size_t n = fread(buffer, 1, SEND_BUFFER_SIZE - 1, stdin);
+  if (n < 0) {
+    perror("Error reading from stdin");
+    return -1;
+  }
+  // Send message to server
+  size_t total_sent = 0;
+  while (total_sent < n) {
+    ssize_t sent = send(sockfd, buffer + total_sent, n - total_sent, 0);
+    if (sent < 0) {
+      perror("Error sending to server");
+      return -1;
+    }
+    total_sent += sent;
+  }
   return 0;
 }
 
 /*
  * main()
  * Parse command-line arguments and call client function
-*/
+ */
 int main(int argc, char **argv) {
   char *server_ip;
   char *server_port;
 
   if (argc != 3) {
-    fprintf(stderr, "Usage: ./client-c [server IP] [server port] < [message]\n");
+    fprintf(stderr,
+            "Usage: ./client-c [server IP] [server port] < [message]\n");
     exit(EXIT_FAILURE);
   }
 
--- assignments/assignment1/client_server/server-c.c
+++ assignments/assignment1/client_server/server-c.c
@@ -24,7 +24,65 @@
  * Return 0 on success, non-zero on failure
 */
 int server(char *server_port) {
-    return 0;
+  // Listen to socket on the given port
+  int port = atoi(server_port);
+  // Check port number 
+  if (port <= 0 || port > 65535) {
+    fprintf(stderr, "Invalid port number: %s\n", server_port);
+    return -1;
+  }
+  // Create socket
+  int sockfd;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    perror("Error opening socket");
+    return -1;
+  }
+  // Bind socket to port
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(port);
+  if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("Error binding socket");
+    return -1;
+  }
+  // Listen for incoming connections
+  if (listen(sockfd, QUEUE_LENGTH) < 0) {
+    perror("Error listening on socket");
+    return -1;
+  }
+  //printf("Server ready, listening on port %d\n", port);
+  // Accept incoming connections
+  while (1) {
+    // Get client connection
+    struct sockaddr_in cli_addr;
+    socklen_t clilen = sizeof(cli_addr);
+    int clisockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    if (clisockfd < 0) {
+      perror("Error accepting connection");
+      continue;
+    }
+    //printf("Client connected\n");
+    // Receive message from client
+    char buffer[RECV_BUFFER_SIZE];
+    ssize_t n = recv(clisockfd, buffer, RECV_BUFFER_SIZE - 1, 0);
+    if (n < 0) {
+      perror("Error reading from socket");
+      close(clisockfd);
+      continue;
+    }
+    buffer[n] = '\0';
+    //printf("Received message: %s\n", buffer);
+    // Write message to stdout
+    fwrite(buffer, 1, n, stdout);
+    fflush (stdout);
+    // Close client socket
+    close(clisockfd);
+  }
+  close(sockfd);
+  return 0;
 }
 
 /*
